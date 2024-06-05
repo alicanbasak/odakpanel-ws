1a16e8ec3ef50b8ee01093d1ab299734142a7bd2
Alican Basak <alicanbasak@icloud.com>
2024-06-05 03:03:29 +0300

    fix rfq search

--- services/rfq.service.js
+++ services/rfq.service.js
@@ -26,10 +26,12 @@ class RfqService {
 
     const whereClauses = [];
 
-    addWhereClause("Search", search, true);
+    // addWhereClause("Search", search, true);
 
     if (search) {
-      whereClauses.push(`Search LIKE '%${search}%'`);
+      whereClauses.push(
+        `(Id LIKE '%${search}%' OR Gerber LIKE '%${search}%' OR OdakCode LIKE '%${search}%' OR CustomerCode LIKE '%${search}%' OR OdakOrderNumber LIKE '%${search}%')`
+      );
     }
 
     const whereClause =
@@ -48,9 +50,46 @@ class RfqService {
       const rfqResult = await pool.request().query(rfqQuery);
       const rfqList = rfqResult.recordset;
 
+      const customerIds = [...new Set(rfqList.map(rfq => rfq.CustomerId))];
+      const rfqStatusIds = [...new Set(rfqList.map(rfq => rfq.RfqStatus))];
+      let customers = {};
+      let rfqStatuses = {};
+
+      console.log("rfqStatusIds", rfqStatusIds);
+      if (customerIds.length > 0) {
+        const customerQuery = `SELECT Id AS CustomerId, Name FROM Customers WHERE Id IN (${customerIds
+          .map(id => `'${id}'`)
+          .join(",")})`;
+
+        const customerResult = await pool.request().query(customerQuery);
+
+        customers = customerResult.recordset.reduce((acc, customer) => {
+          acc[customer.CustomerId] = customer.Name;
+          return acc;
+        }, {});
+      }
+
+      if (rfqStatusIds.length > 0) {
+        const rfqStatusQuery = `SELECT Id AS RfqStatus, Name FROM RfqStatus WHERE Id IN (${rfqStatusIds
+          .map(id => `'${id}'`)
+          .join(",")})`;
+        console.log("rfqStatusQuery", rfqStatusQuery);
+        const rfqStatusResult = await pool.request().query(rfqStatusQuery);
+        console.log("rfqStatusResult", rfqStatusResult);
+        rfqStatuses = rfqStatusResult.recordset.reduce((acc, status) => {
+          acc[status.RfqStatus] = status.Name;
+          return acc;
+        }, {});
+      }
+
+      const rfqsWithCustomerNamesAndSatatusName = rfqList.map(rfq => ({
+        ...rfq,
+        CustomerName: customers[rfq.CustomerId] || null,
+        RfqStatusName: rfqStatuses[rfq.RfqStatus] || null,
+      }));
       return {
         totalCount,
-        items: rfqList,
+        items: rfqsWithCustomerNamesAndSatatusName,
       };
     } catch (error) {
       throw new Error(error.message);
